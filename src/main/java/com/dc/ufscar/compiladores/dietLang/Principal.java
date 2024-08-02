c221f299b625c893298da46433d715f82e3a3371
matheusymm <matheusyuitimoriy@gmail.com>
2024-08-02 10:51:06 -0300

    first commit

---
+++ src/main/java/com/dc/ufscar/compiladores/dietLang/Principal.java
@@ -0,0 +1,13 @@
+package com.dc.ufscar.compiladores.dietLang;
+
+/**
+ * Hello world!
+ *
+ */
+public class Principal 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
